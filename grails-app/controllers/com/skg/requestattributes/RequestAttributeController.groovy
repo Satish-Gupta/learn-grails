85c9e7a74f4ce2c7dad76b62d85aa2b5ce32c59e
satish <satishgupta@lftechnology.com>
2015-12-07 23:39:59 +0545

    request-attributes: attribute manipulation in servlet vs grails controller

--- grails-app/controllers/com/skg/requestattributes/RequestAttributeController.groovy
+++ grails-app/controllers/com/skg/requestattributes/RequestAttributeController.groovy
@@ -3,6 +3,8 @@ package com.skg.requestattributes
 class RequestAttributeController {
 
     def index() {
+    }
+    def displayRequestAttributes() {
         String requestAttributes = """      actionName: $actionName
                         params: $params
                         actionUri: $actionUri
@@ -16,7 +18,7 @@ class RequestAttributeController {
         log.info requestAttributes
 
         render """<pre>\t\t\tactionName: $actionName
-                        params: $params
+                        params: $params  params type: is params map:${params instanceof Map}
                         actionUri: $actionUri
                         controllerUri:$controllerUri
                         flash: $flash
@@ -25,5 +27,48 @@ class RequestAttributeController {
                         response: $response
                         session:$session
                         servletContext: $servletContext</pre>"""
+    }
+
+    def oldVsNew() {
+        request.setAttribute("myRequestAttr", "myRequestValue1111");
+        request.yourRequestAttr = "yourRequestValue2222"
+
+        log.info 'request.getAttribute("myRequestAttr")  = ' + request.getAttribute("myRequestAttr");
+        log.info 'request.yourRequestAttr = ' + request.yourRequestAttr
+
+        session.setAttribute("mySessionAttr", "mySessionValue3333");
+        session.yourSessionAttr = "yourSessionValue4444"
+
+        log.info 'session.getAttribute("mySessionAttr") = ' + session.getAttribute("mySessionAttr");
+        log.info 'session.yourSessionAttr = ' + session.yourSessionAttr
+
+        servletContext.setAttribute("myServletContextAttr", "myServletContextValue5555");
+        servletContext.yourServletContextAttr = "yourServletContextValue6666"
+
+        log.info 'servletContext.getAttribute("myServletContextAttr"); = ' + servletContext.getAttribute("myServletContextAttr");
+        log.info 'servletContext.yourServletContextAttr = ' + servletContext.yourServletContextAttr
+
+        String string = """<pre>
+            <code>request.setAttribute("myRequestAttr", "myRequestValue1111");</code>
+            request.yourRequestAttr = "yourRequestValue2222"
+
+        request.getAttribute("myRequestAttr")  Output==>  ${request.getAttribute("myRequestAttr");}
+        request.yourRequestAttr Output==> $request.yourRequestAttr
+
+        session.setAttribute("mySessionAttr", "mySessionValue3333");
+        session.yourSessionAttr = "yourSessionValue4444"
+
+        session.getAttribute("mySessionAttr") Output==> ${session.getAttribute("mySessionAttr");}
+        session.yourSessionAttr Output==> $session.yourSessionAttr
+
+        servletContext.setAttribute("myServletContextAttr", "myServletContextValue5555");
+        servletContext.yourServleContextAttr == "yourServletContextValue6666"
+
+        servletContext.getAttribute("myServletContextAttr"); Output==> ${servletContext.getAttribute("myServletContextAttr");}
+        servletContext.yourServletContextAttr Output==> $servletContext.yourServletContextAttr
+</pre>
+        """
+
+        render string
     }
 }
---
+++ grails-app/views/requestAttribute/index.gsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: satish
+  Date: 12/7/15
+  Time: 11:26 PM
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title></title>
+</head>
+
+<body>
+    <g:link controller="requestAttribute" action="displayRequestAttributes">
+        Display Request Attributes
+    </g:link>
+    <br/>
+    <g:link controller="requestAttribute" action="oldVsNew">
+        Display request attribute manipulation between servlet and grails controller
+    </g:link>
+</body>
+</html>
