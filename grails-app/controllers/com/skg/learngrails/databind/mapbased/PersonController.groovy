fa05e114fd44df1d4998c2fa4564d89289a49d03
satish <satishgupta@lftechnology.com>
2016-01-06 00:34:55 +0545

    simple-map - illustrate simple map based data-bind

---
+++ grails-app/controllers/com/skg/learngrails/databind/mapbased/PersonController.groovy
@@ -0,0 +1,90 @@
+package com.skg.learngrails.databind.mapbased
+
+
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class PersonController {
+
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Person.list(params), model:[personInstanceCount: Person.count()]
+    }
+
+    def show(Person personInstance) {
+        respond personInstance
+    }
+
+    def create() {
+        respond new Person(params)
+    }
+
+    @Transactional
+    def save() {
+        def bindingMap = [firstName: params.firstName, lastName: params.lastName, age: params.age]
+        def personInstance = new Person(bindingMap)
+        personInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'person.label', default: 'Person'), personInstance.id])
+                redirect personInstance
+            }
+            '*' { respond personInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(Person personInstance) {
+        respond personInstance
+    }
+
+    @Transactional
+    def update() {
+
+        def bindingMap = [firstName: params.firstName, lastName: params.lastName, age: params.age]
+        def personInstance = Person.get(params.id)
+        personInstance.properties = bindingMap
+        personInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'Person.label', default: 'Person'), personInstance.id])
+                redirect personInstance
+            }
+            '*'{ respond personInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Person personInstance) {
+
+        if (personInstance == null) {
+            notFound()
+            return
+        }
+
+        personInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Person.label', default: 'Person'), personInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'person.label', default: 'Person'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+}
---
+++ grails-app/domain/com/skg/learngrails/databind/mapbased/Person.groovy
@@ -0,0 +1,9 @@
+package com.skg.learngrails.databind.mapbased
+
+class Person {
+        String firstName
+        String lastName
+        Integer age
+    static constraints = {
+    }
+}
